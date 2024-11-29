c774ce2bb95870e1e72a872af17b9de7fab9a78b
alvinle2901 <bach938@gmail.com>
2024-11-29 10:19:06 +0100

    Add /35mm button to floating dock

--- src/components/floating-dock.tsx
+++ src/components/floating-dock.tsx
@@ -9,6 +9,8 @@ import { LucideIcon, Menu } from 'lucide-react';
 
 import { cn } from '@/utils/cn';
 
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from './ui/tooltip';
+
 export const FloatingDock = ({
   items,
   className,
@@ -18,48 +20,57 @@ export const FloatingDock = ({
 }) => {
   const [open, setOpen] = useState(false);
   return (
-    <div className={cn('relative block md:hidden', className)}>
-      <AnimatePresence>
-        {open && (
-          <motion.div
-            layoutId="nav"
-            className="absolute inset-x-0 bottom-full mb-2 flex flex-col gap-2"
-          >
-            {items.map((item, idx) => (
-              <motion.div
-                key={item.title}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                }}
-                exit={{
-                  opacity: 0,
-                  y: 10,
-                  transition: {
-                    delay: idx * 0.05,
-                  },
-                }}
-                transition={{ delay: (items.length - 1 - idx) * 0.05 }}
-              >
-                <Link
-                  href={item.href}
+    <TooltipProvider>
+      <div className={cn('relative block md:hidden', className)}>
+        <AnimatePresence>
+          {open && (
+            <motion.div
+              layoutId="nav"
+              className="absolute inset-x-0 bottom-full mb-2 flex flex-col gap-2"
+            >
+              {items.map((item, idx) => (
+                <motion.div
                   key={item.title}
-                  className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-900"
+                  initial={{ opacity: 0, y: 10 }}
+                  animate={{
+                    opacity: 1,
+                    y: 0,
+                  }}
+                  exit={{
+                    opacity: 0,
+                    y: 10,
+                    transition: {
+                      delay: idx * 0.05,
+                    },
+                  }}
+                  transition={{ delay: (items.length - 1 - idx) * 0.05 }}
                 >
-                  <item.icon className="h-5 w-5 text-neutral-400" />
-                </Link>
-              </motion.div>
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
-      <button
-        onClick={() => setOpen(!open)}
-        className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-800"
-      >
-        <Menu className="size-5 text-neutral-400" />
-      </button>
-    </div>
+                  <Tooltip>
+                    <TooltipTrigger asChild>
+                      <Link
+                        href={item.href}
+                        key={item.title}
+                        className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-900"
+                      >
+                        <item.icon className="h-5 w-5 text-neutral-400" />
+                      </Link>
+                    </TooltipTrigger>
+                    <TooltipContent>
+                      <p>{item.title}</p>
+                    </TooltipContent>
+                  </Tooltip>
+                </motion.div>
+              ))}
+            </motion.div>
+          )}
+        </AnimatePresence>
+        <button
+          onClick={() => setOpen(!open)}
+          className="flex h-10 w-10 items-center justify-center rounded-full bg-neutral-800"
+        >
+          <Menu className="size-5 text-neutral-400" />
+        </button>
+      </div>
+    </TooltipProvider>
   );
 };
